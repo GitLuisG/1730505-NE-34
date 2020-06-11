987c81d91e407cb55759c2942984d8e8d3c72f61
Luis Gerardo Perales Torres <1730505@upv.edu.mx>
2020-06-11 13:33:09 -0500

    nuevoCambio

--- N-E/wp-content/themes/1589867191-tg/template-parts/footer/site-info.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Displays footer site info
- */
-
-?>
-<div class="site-info-tgen">
-	Built with <a href="https://themesgenerator.com/" title="Create & edit Wordpress Themes">WordPress Themes Generator</a>
-</div><!-- .site-info -->
